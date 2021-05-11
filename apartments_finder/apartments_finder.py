16f748eb96471d5ddd927197afdd959951f2b3f4
zackdreitzler <zdreitzler17@gmail.com>
2021-05-10 19:48:05 -0700

    Added filters dict

--- apartments_finder/apartments_finder.py
+++ apartments_finder/apartments_finder.py
@@ -7,4 +7,5 @@ __status__ = 'Prototype'
 from data_collection.apartmentsdotcom import get_apartmentsdotcom_data
 
 if __name__ == '__main__':
-    get_apartmentsdotcom_data()
+    filters = {'bedrooms': [0, 1], 'location': 'Washington, DC'}
+    get_apartmentsdotcom_data(filters=filters)
