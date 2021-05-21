699c6f9c024050d0c431ce90ee0089c286bd4831
zackdreitzler <zdreitzler17@gmail.com>
2021-05-20 18:29:34 -0700

    Added apartmentsdata route

--- apartments_finder/apartments_finder.py
+++ apartments_finder/apartments_finder.py
@@ -4,15 +4,25 @@ __license__ = 'MIT'
 __version__ = '0.0.1'
 __status__ = 'Prototype'
 
-import flask
-from flask import Flask
+from data_collection import apartmentsdotcom
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+@app.route('/apartmentsdata')
+def get_data():
+    location = 'Philadelphia, PA'
+    bedrooms = [1]
+    filters = {'location': location,
+               'bedrooms': bedrooms}
+    data = apartmentsdotcom.get_apartmentsdotcom_data(filters)
+    return render_template('apartmentsdata.html',  tables=[data.to_html(classes='data')], titles=data.columns.values)
 
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
