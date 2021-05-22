b0d0c76f23f35ae3a741cd3f72a8ff0602a103db
zackdreitzler <zdreitzler17@gmail.com>
2021-05-21 19:39:07 -0700

    Added form to index and generate filters on get_data

--- apartments_finder/apartments_finder.py
+++ apartments_finder/apartments_finder.py
@@ -4,24 +4,40 @@ __license__ = 'MIT'
 __version__ = '0.0.1'
 __status__ = 'Prototype'
 
+
 from data_collection import apartmentsdotcom
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/apartmentsdata')
+@app.route('/apartmentsdata', methods=['GET', 'POST'])
 def get_data():
-    location = 'Philadelphia, PA'
-    bedrooms = [1]
-    filters = {'location': location,
-               'bedrooms': bedrooms}
+    """
+    Gets data from apartmentsdotcom and passes it to the apartmentsdata page
+    :return: rendered html page with apartments data
+    """
+    filters = {'location': request.form['city_filter'],
+               'bedrooms': [int(request.form['bed_filter'])]}
     data = apartmentsdotcom.get_apartmentsdotcom_data(filters)
-    return render_template('apartmentsdata.html',  tables=[data.to_html(classes='data')], titles=data.columns.values)
+    tables = []
+    titles = []
+    if not data.empty:
+        tables = [data.to_html()]
+        titles = data.columns.values
+    return render_template('apartmentsdata.html',  tables=tables, titles=titles)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """
+    Returns the index page of the website.
+    :return: rendered html page of the index page.
+    """
+
+    with open('./config/usa_cities') as cities_file:
+        cities = cities_file.readlines()
+
+    return render_template('index.html', cities=cities)
 
 
 if __name__ == '__main__':
